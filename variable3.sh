7728e6b54dd2bd9ca0dda02c985c1f336db68c5a
lokanarayana <lokanarayanak@gmail.com>
2023-05-21 16:40:22 +0530

    Variable

---
+++ variable3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Ram
+PERSON2=Suresh
+echo "${PERSON1}: Hi ,${PERSON2}"
+echo "${PERSON2}: Hi ,${PERSON1}"
+echo "${PERSON1}: Good Morning ${PERSON2}"
+
+echo "${PERSON2}: vVGM ${PERSON1}"
