1553ea961f8c55c1d95f072d299f413b59bc851a
lokanarayana <lokanarayanak@gmail.com>
2023-05-21 18:28:20 +0530

    Variable

---
+++ ExitStatus/gitinsatll.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id-u)
+#check user is root or not
+
+if [ $USERID -ne 0 ] ; then
+    echo "You need to be a root user to execute this script"
+    exit 1
+fi
+
+yum install mysql-server -y
+
+if [ $? -ne 0 ] ; then
+    echo "SQL server insatll failed"
+    exit 1
+else 
+    echo "SQL server insatll success"
+fi
