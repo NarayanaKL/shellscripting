cee9f79ef9a85be18ae7ea3c3b5b97a208f60cc6
lokanarayana <lokanarayanak@gmail.com>
2023-05-21 18:54:36 +0530

    Variable

---
+++ Functions/function1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Hello () {
+   echo "Hello !!! $1"
+   echo "script Name: $0"
+   echo "Number of args: $#"
+   echo "All args are : $@"
+}
+
+echo "Before calling function,checking name : $1"
+Hello $1
